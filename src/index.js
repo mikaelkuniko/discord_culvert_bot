eecb34250827d28a74500879185318b749dcede9
mikaelkuniko <mikaelkuniko@gmail.com>
2024-04-02 13:30:29 -0700

    created bot intents

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 const {Client, IntentsBitField} = require('discord.js');
 
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds, 
+        IntentsBitField.Flags.GuildMembers, 
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent
+    ]
+});
+
