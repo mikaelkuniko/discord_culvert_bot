f5d52ecf93bebafad3d3c638f0aaa9d52c203fad
mikaelkuniko <mikaelkuniko@gmail.com>
2024-04-10 09:11:07 -0700

    changed imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import properties from '../src/properties.js'
+// import properties from '../src/properties.js'
 
-import { Client, IntentsBitField } from 'discord.js';
+// import { Client, IntentsBitField } from 'discord.js';
+
+require('dotenv').config()
+const {Client, IntentsBitField} = require('discord.js')
 
 const client = new Client({
     intents: [
@@ -16,13 +19,20 @@ client.on('ready', (c) => {
 })
 
 client.on('messageCreate', (message)=> {
+    if(message.author.bot) return
     console.log(message)
+    console.log("This is the message content: ", message.content);
+    if(message.content){
+        message.reply("Hello I am a bot")
+    }
     if(message.content == "activate"){
         message.reply("Active and ready to obey");
     }
 })
 
-client.login(properties.DISCORD_KEY);
+// client.login(properties.DISCORD_KEY);
+
+client.login(process.env.TOKEN)
 
 
 
