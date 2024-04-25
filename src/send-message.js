4f9d678ec50d0e7c73ed016f7f24afb3dac3b003
mikaelkuniko <mikaelkuniko@gmail.com>
2024-04-25 07:39:56 -0700

    created roles

---
+++ src/send-message.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const {Client, IntentsBitsField, ActionRowBuilder, ButtonBuilder, ButtonStyle} = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds, 
+        IntentsBitField.Flags.GuildMembers, 
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent
+    ]
+});
+
+const roles = [
+    { 
+        id: '1233063150291652639',
+        label: "Black"
+    },
+    { 
+        id: '1233063281086959658',
+        label: "White"
+    },
+]
+
+client.on('ready', async (c) => {
+    try{
+        const channel = await client.channel.cache.get('1224809783274508443');
+        if(!channel) return;
+        
+        const row = new ActionRowBuilder()
+
+        roles.forEach((role) => {
+            row.components.push(
+                new ButtonBuilder().setCustomId(role.id).setLabel(role.label).setStyle(ButtonStyle.Primary)
+            )
+        });
+
+    await channel.send({
+        content: 'Claim or remove a role below.',
+        components: [row],
+    });
+
+    } catch(error) {
+        console.log(error);
+    }
+});
+process.exit();
+client.login(process.env.TOKEN);
