e2f7669c957209a6418005b7e14ae2090250e313
mikaelkuniko <mikaelkuniko@gmail.com>
2024-06-18 11:48:10 -0700

    created route for testing

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000; // Use the PORT environment variable or default to 3000
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
