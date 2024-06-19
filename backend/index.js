068327e8c6e109060df0771f9910fd1f7c91898b
mikaelkuniko <mikaelkuniko@gmail.com>
2024-06-19 14:46:20 -0700

    created routes for backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const router = express.Router();
+const apiRouter = require('./api')
 const app = express();
 const port = process.env.PORT || 3000; // Use the PORT environment variable or default to 3000
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
