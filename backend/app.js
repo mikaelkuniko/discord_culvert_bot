6380eb88255bf5c800a37fbff7e2b0de238c1411
mikaelkuniko <mikaelkuniko@gmail.com>
2024-07-25 14:49:46 -0700

    error handling for routes

--- backend/app.js
+++ backend/app.js
@@ -5,3 +5,18 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+const { ValidationError } = require('sequelize');
+
+const app = express();
+
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+  });
