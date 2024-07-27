07442804823fdcebbc0cf1788116b5bc56351252
mikaelkuniko <mikaelkuniko@gmail.com>
2024-07-27 10:40:44 -0700

    error handling for server errors

--- backend/app.js
+++ backend/app.js
@@ -29,3 +29,14 @@ app.use((_req, _res, next) => {
     }
     next(err);
   });
+
+  app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+  });
