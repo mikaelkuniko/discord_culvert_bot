17b6e7f6a42773b944d109a3f495cd92b6bc18f9
mikaelkuniko <mikaelkuniko@gmail.com>
2024-08-09 16:37:56 -0700

    culvert score user route created

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -3,4 +3,30 @@ const router = express.Router();
 
 const { User } = require('../../db/models')
 
+// culvert post endpoint
+router.post(
+    '/',
+    async (req, res, next) => {
+        const {user, score} = req.body
+
+        let checkUser = await User.findOne({
+            where: {
+                user: req.body.user
+            }
+        })
+
+        if(!user){
+            res.status(400);
+            return res.json({
+                "message": "Validation error",
+                "statusCode": 400,
+                "errors": {
+                    "user": "Invalid user"
+                }
+            })
+        }
+
+    }
+)
+
 module.exports = router;
